39464cc0b1180f603565d4e9c076feb56310c291
Daniel Martel <danielmartelmarrero@gmail.com>
2024-07-19 21:01:33 +0100

    Done until Iteration 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
+import ProfileImage from "./components/ProfileImage";
 
 const tweetsArray = [
   {
@@ -37,7 +38,8 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+    /*<ProfileImage tweet={tweetsArray[0]}/>*/
+      <Tweet tweet={tweetsArray[0]} />
     </div>
   );
 }
